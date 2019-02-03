5fc0a7e95cb03b89f603f3a5aa0fac969a96d861
Dylan Pugh <pughdylan93@gmail.com>
2019-02-02 20:57:47 -0500

    LiriHW

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LiriBot
+
+This is my Liri Bot
+
+It functions using node.js in your terminal/bash window
+
+To use Liri, simply code in "node liri.js spotify-this" plus a song of choice and see the results,
+or you can also type "node liri.js movie-this" plus title of a movie, or " node liri.js concert-this" or "node liri.js do-what-it-says"
--- liri.js
+++ liri.js
@@ -18,7 +18,11 @@ if(command === "spotify-this-song") {
 
     spotify.search({type: "track", query: item})
     .then(function(response) {
-        console.log("Artist: " + response.tracks.items[0].artists[0].name + "\nName of Song: " + response.tracks.items[0].name + "\nAlbum: " + response.tracks.items[0].album.name + "\nURL: " + response.tracjs.items[0].preview_url + divider);
+        var song = "Artist: " + response.tracks.items[0].artists[0].name + "\nName of Song: " + response.tracks.items[0].name + "\nAlbum: " + response.tracks.items[0].album.name + "\nURL: " + response.tracjs.items[0].preview_url + divider;
+        console.log(song);
+
+        writeToLog(song);
+
     }).catch(function(err) {
         console.log(err);
     });
@@ -31,7 +35,10 @@ if(command === "movie-this") {
 
     axios.get("http://www.omdbapi.com/?t=" + item + "&y=&plot+short&apikey=trilogy")
     .then(function(response) {
-        console.log("Movie Title: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: " +response.data.Ratings[2].Value + "\nCountry: " + response.data.Country + "\nSummary: " + response.data.Plot + "\nActors/Actresses: " + response.data.Actors);
+        var movie = "Movie Title: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: " +response.data.Ratings[2].Value + "\nCountry: " + response.data.Country + "\nSummary: " + response.data.Plot + "\nActors/Actresses: " + response.data.Actors;
+        console.log(movie);
+
+        writeToLog(movie);
 
     });
 }
@@ -44,7 +51,50 @@ if(command === "concert-this") {
     axios.get("https://rest.bandsintown.com/artists/" + item + "/events?app_id=codingbootcamp")
     .then(function(response) {
         for(var i = 0; i < response.data.length; i++) {
-            console.log("Venue: " + response.data[i].venue.name + "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.region + "\nDate: " + moment(response.data[i].datetime).format("LLLL") + divider);
+            var concert = "Venue: " + response.data[i].venue.name + "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.region + "\nDate: " + moment(response.data[i].datetime).format("LLLL") + divider
+            console.log(concert);
+
+            writeToLog(concert);
+            
         }
+    });
+}
+
+if(command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf-8", function(error, data) {
+
+        if(error) {
+            return console.log(error);
+        }
+
+        var array = data.split(",");
+
+        name = array[1]
+
+        spotify.search({type: "track", query: name, limit: 1}, function(err, data) {
+            if(err) {
+                return console.log("Error ocurred: " + err);
+            }
+
+            var track = data.tracks.items[0];
+
+            var randSong = "Song Title: " + name + "\nArtist: " + track.artists[0].name + "\nAlbum: " + track.album.name + "\nLink: " + track.preview_url + divider;
+
+            console.log(randSong);
+
+            writeToLog(randSong);
+
+        })
+
     })
 }
+
+function writeToLog(printInfo) {
+	fs.appendFile("log.txt", printInfo, function(err) {
+
+		// If the code experiences any errors it will log the error to the console.
+		if (err) {
+			return console.log(err);
+		}
+
+	})}
